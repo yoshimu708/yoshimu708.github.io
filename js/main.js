7d29a0ed6c8ef8fb6d8f4915ae06b5e8efddf50d
yoshimu708 <73528183+yoshimu708@users.noreply.github.com>
2021-02-18 23:10:23 +0900

    デザイン・内容の編集

--- js/main.js
+++ js/main.js
@@ -1,25 +1,7 @@
 'user strict';
 
 {
-  // document.getElementById("task").addEventListener('mouseover',()=>{
-  //   document.getElementById("task-sent").style.display="block";
-  // },false);
-  // document.getElementById("task").addEventListener('mouseout',()=>{
-  //   document.getElementById("task-sent").style.display="none";
-  // },false);
 
-  // document.getElementById("furima").addEventListener('mouseover',()=>{
-  //   document.getElementById("furima-sent").style.display="block";
-  // },false);
-  // document.getElementById("furima").addEventListener('mouseout',()=>{
-  //   document.getElementById("furima-sent").style.display="none";
-  // },false);
-
-  // document.getElementById("games").addEventListener('mouseover',()=>{
-  //   document.getElementById("games-sent").style.display="block";
-  // },false);
-  // document.getElementById("games").addEventListener('mouseout',()=>{
-  //   document.getElementById("games-sent").style.display="none";
-  // },false);
+  
 
 }
