c949259eb06cda1692c0463cec4332f96b3f6b11
yoshimu708 <73528183+yoshimu708@users.noreply.github.com>
2021-02-18 23:35:20 +0900

    アコーディオンメニューの作成

--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@
 
 {
 
-  
+  const menu = document.querySelectorAll(".open-menu");
+
+  function toggle() {
+    const content = this.nextElementSibling;
+    this.classList.toggle("is-active");
+    content.classList.toggle("is-open");
+  }
+
+  for (let i = 0; i < menu.length; i++) {
+    menu[i].addEventListener("click", toggle);
+  }
 
 }
